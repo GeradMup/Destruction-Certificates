ba557eac8e61ed6bb93b414ba2a80e4ca2b9b3dd
Gerad Mupfumisi <gerrym751@gmail.com>
2025-03-27 15:59:10 +0200

    Working version of the PDF generator with a lot of Technical Debt

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.pdfmetrics import stringWidth
 from reportlab.lib import colors
 from stringHelpers import *
+from datetime import datetime
 
 
 def pdfGen():
@@ -16,25 +17,31 @@ def pdfGen():
     paths = pathsFile.readlines()
     outputFolder = afterDelimeter(paths[0], "|")
 
-    #read all inputs here
-    dateOfCollection = "21 February 2025"
-
-
     print(outputFolder)
 
-    fileName = outputFolder + '\\sample.pdf'
-    documentTitle = 'sample'
-    title = 'Destruction Certificate'
+    #Inputs
+    logo = resourcesDir + "Logo.png"
+    signature = resourcesDir + "Green Enviro Signature.jpeg"
+    date = datetime.today().strftime('%d %B %Y')
+    company = "Technologia Group"
+    contactPerson = "Isak Van Wyk"
+    email = "isak.vanwyk@technologiagroup.com"
+    contact = "081 016 1876"
+    dateOfCollection = input("Enter Collection Date:").strip()
+    productDescription = input("Enter Product Description:").strip()
+    quantity = input("Enter product quantity in pallets:").strip() + " Pallets"
 
-    image = resourcesDir + "Logo.png"
+    fileName = outputFolder + f'\\ {company} {date}'
+    path = filePath(fileName)
     
     # creating a pdf object 
-    pdf = canvas.Canvas(fileName) 
+    pdf = canvas.Canvas(path) 
     
     # Add the LOGO first   
-    pdf.drawImage(image, x= 25, y= 690, width=540, height=120, mask=None) 
+    pdf.drawImage(logo, x= 25, y= 690, width=540, height=120, mask=None) 
 
     # setting the title of the document 
+    documentTitle = 'DS'
     pdf.setTitle(documentTitle) 
     
     # registering a external font in python 
@@ -44,17 +51,18 @@ def pdfGen():
     
     # creating the title by setting it's font  
     # and putting it on the canvas 
+    title = 'Destruction Certificate'
     pdf.setFont('Times-Bold', 25) 
     pdf.drawCentredString(300, 635, title) 
     xPos = 50
     yPos = 570
     yOffset = 15
 
-    specialLine(pdf, title="Date", description="27 February 2025", x=xPos, y=yPos)
-    specialLine(pdf, title="Company", description="Technologia Group", x=xPos, y=yPos - yOffset)
-    specialLine(pdf, title="Contact Person", description="Isak Van Wyk", x=xPos, y=yPos - 2*yOffset)
-    specialLine(pdf, title="Email Address", description="isak.vanwyk@technologiagroup.com", x=xPos, y=yPos - 3*yOffset)
-    specialLine(pdf, title="Contact Numbers", description="081 016 1876", x=xPos, y=yPos - 4*yOffset)
+    specialLine(pdf, title="Date", description=date, x=xPos, y=yPos)
+    specialLine(pdf, title="Company", description=company, x=xPos, y=yPos - yOffset)
+    specialLine(pdf, title="Contact Person", description=contactPerson, x=xPos, y=yPos - 2*yOffset)
+    specialLine(pdf, title="Email Address", description=email, x=xPos, y=yPos - 3*yOffset)
+    specialLine(pdf, title="Contact Numbers", description=contact, x=xPos, y=yPos - 4*yOffset)
 
     textLines = [ 
         f'This is to certify that all products collected from your premises on {dateOfCollection}', 
@@ -70,9 +78,8 @@ def pdfGen():
     pdf.drawText(text) 
     
     yPos = 405
-    specialLine(pdf, title="Description of Products", description="DSTV TOP COVERS", x=xPos, y=yPos)
-    specialLine(pdf, title="Quantity", description="3 Pallets", x=xPos, y=yPos - yOffset)
-    
+    specialLine(pdf, title="Description of Products", description=productDescription, x=xPos, y=yPos)
+    specialLine(pdf, title="Quantity", description=quantity, x=xPos, y=yPos - yOffset)
 
     #Disclaimer
     disclaimer = [ 
@@ -87,15 +94,21 @@ def pdfGen():
         text.textLine(line)
     pdf.drawText(text)
 
-
+    #Add Signature
+    pdf.drawImage(signature, x=xPos, y= 200, width=150, height=50, mask=None)
     
-
+   
+    pdf.setFont("Times-Bold", fontSize)             
+    pdf.drawString(x=xPos, y=190, text=f"Operations Director                                                                           {date}")
+    pdf.drawString(x=xPos, y=175, text="Nicholas Mupfumisi")
 
     # saving the pdf 
     pdf.save() 
 
     print("Certificate Generated!")
     return
+
+#---------------------------------------------------------------------------------------------------------
 
 def specialLine(pdfObject, title, description, x, y):
     fontSize = 13
@@ -108,6 +121,7 @@ def specialLine(pdfObject, title, description, x, y):
     pdfObject.drawString(x, y, description)
     return
 
+#---------------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     pdfGen()
--- src/stringHelpers.py
+++ src/stringHelpers.py
@@ -1,5 +1,22 @@
+import os
+
 def afterDelimeter(string, delimeter):
     splitedString = string.split(delimeter)
     pathString = splitedString[1]
     pathString = pathString.replace('\n','')
     return pathString
+
+def filePath(titlePath, number=0):
+
+    checkPath=""
+    if number == 0:
+        checkPath = titlePath + ".pdf"
+    else:
+        checkPath = titlePath + "_" + str(number) + ".pdf"
+    
+    
+    if os.path.exists(checkPath):
+        return filePath(titlePath, number + 1)
+    
+    else:
+        return checkPath
